fa07cd24650f783407a590af5892284eb40d3842
lonestarb <brendiniads@gmail.com>
2025-01-18 22:06:56 +0000

    commit --amend pages/

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+function Home() {
+    return <h1>Olá, irmão! esse é um site que eu criei sem gastar um sentado para deixa-lo online 
+        pois o meu próprio computador processa o site, incluindo informações que ele recebe e envia da e para a internet para manter o site acessível em qualquer lugar que tenha acesso a internet no mundo, 
+        agora me sinto vivo na era da informação.</h1>
+}
+
+export default Home;
