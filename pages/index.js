78267558d2293baa120efca677383239556a0b58
lonestarb <brendiniads@gmail.com>
2025-01-22 01:22:03 +0000

    amend pro meu irmão ver

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
 function Home() {
-    return <h1>Olá, irmão! esse é um site que eu criei sem gastar um sentado para deixa-lo online 
-        pois o meu próprio computador processa o site, incluindo informações que ele recebe e envia da e para a internet para manter o site acessível em qualquer lugar que tenha acesso a internet no mundo, 
-        agora me sinto vivo na era da informação.</h1>
+    return <h1>Olá, irmão! eu sou feliz mas as vezes eu esqueço, se você também esquece da uma risadinha! :) esse site eu criei no diretorio do github
+        para fins de aprendizado e publiquei gratuitamente no servidor da vercel, sabia que da pra mim 
+        deixar meu site online usando meu próprio computador como servidor? 
+        eu ainda não aprendi a fazer isso mas quase pelomenos aprendi que da pra aprender hehe sorria de novo 
+        pra mim sentir que fazer um site ta sendo útil.
+        pronto atualizei.
+    </h1>
 }
 
 export default Home;
