44260868ccd2119caa4f38f864fb425aee8bbe4e
lonestarb <brendiniads@gmail.com>
2025-06-20 02:19:15 +0000

    pequena mudança no conteúdo da página home, e acho que o prettier pode também ter alterado package-lock.json

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,5 @@
 function Home() {
-  return (
-    <h1>
-      Olá, determinado! O projeto será ajudar as pessoas a ser quem são a
-      qualquer momento e em qualquer lugar. O projeto deve ter em mente que
-      qualquer palavra com intenção de ajudar as pessoas a ser, deve ser
-      substituída por uma mesma palavra em repetitiva que é a ser. Por exemplo,
-      a palavra "eu" deve ser substituida por ser, a "determinado" também, essa
-      substituição deve ser priorizada acima de qualquer coisa e em qualquer
-      lugar mesmo que seja estranho e desconfortável a princípio mas também será
-      necessário encontrar mecanismos de tornar essas trocas e repetições
-      agradáveis para os clientes do site ao longo do projeto.
-    </h1>
-  );
+  return <h1>Estou criando o que eu quiser. Eu gosto de comer carne.</h1>;
 }
 
 export default Home;
