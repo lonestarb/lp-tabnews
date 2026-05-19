8550a60c581783cc8e351e6393cdc2688334f17c
lonestarb <194744596+lonestarb@users.noreply.github.com>
2026-05-19 14:54:11 +0000

    makes `database.js` `cliente.connect()` more robust to errors

--- infra/database.js
+++ infra/database.js
@@ -1,5 +1,5 @@
 import { Client } from "pg";
-import { client_encoding } from "pg/lib/defaults";
+import { client_encoding, host } from "pg/lib/defaults";
 
 async function query(queryObject) {
   const client = new Client({
@@ -9,13 +9,20 @@ async function query(queryObject) {
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   });
-  await client.connect();
+  console.log("credenciais do postgres:", {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+  });
 
   try {
+    await client.connect();
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
     console.error(error);
+    throw error;
   } finally {
     await client.end();
   }
